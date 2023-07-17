d1f9f4f7857c9188d0b8091b59f93115c0d384c9
Vladimir <ostapenkov74@gmail.com>
2023-07-17 18:39:59 +0500

    Add create endpoint

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -6,17 +6,30 @@ class WorkspacesController < ApplicationController
   end
 
   def new
+    @workspace = Workspace.new
   end
 
   def show
   end
 
   def create
+    @workspace = Workspace.new(workspace_params).merge(author: current_user)
+    if @workspace.save
+      redirect_to @workspace, notice: 'Your workspace successfully created.'
+    else
+      render :new
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def workspace_params
+    params.require(:workspace).permit(:title)
   end
 end
