b67cda8c2e0505fcfcb9f0338bfaccde3216be65
Vladimir <ostapenkov74@gmail.com>
2023-07-17 13:05:04 +0500

    Fix association

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :threads, dependent: :destroy
-  has_many :users, through: :threads, dependent: :destroy
+  has_many :members, dependent: :destroy
+  has_many :threads, through: :members, dependent: :destroy
 end
