25b6a115688758bb56774da16b377e2927ac336b
Vladimir <ostapenkov74@gmail.com>
2023-07-21 17:17:35 +0500

    Add turbo update for create

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -3,7 +3,7 @@ class WorkspacesController < ApplicationController
   before_action :set_workspase, only: %i[show update destroy]
 
   def index
-    @workspaces = current_user.workspaces
+    @workspaces = workspaces
   end
 
   def new
@@ -16,10 +16,21 @@ class WorkspacesController < ApplicationController
 
   def create
     @workspace = Workspace.new(workspace_params.merge(author: current_user))
-    if @workspace.save
-      redirect_to @workspace, notice: 'Your workspace successfully created.'
-    else
-      render :new
+    respond_to do |format|
+      if @workspace.save
+        format.turbo_stream do
+          render turbo_stream: [turbo_stream.update('workspaces',
+                                                    partial: 'workspaces/workspaces',
+                                                    locals: { :workspaces => workspaces }),
+                                                    turbo_stream.update('notice',
+                                                    'Your workspace successfully created.')]
+
+        end
+        format.html { redirect_to @workspace, notice: 'Your workspace successfully created.' }
+      else
+        format.turbo_stream
+        format.html { render :new, status: :unprocessable_entity }
+      end
     end
   end
 
@@ -37,6 +48,10 @@ class WorkspacesController < ApplicationController
   end
 
   private
+
+  def workspaces
+    workspaces = current_user.workspaces
+  end
 
   def set_workspase
     @workspace = Workspace.find(params[:id])
