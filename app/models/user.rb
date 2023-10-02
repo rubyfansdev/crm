c2488e671a11286c324a358d5367396018c04933
Victor Malukhin <victor.malukhin@gmail.com>
2023-10-02 20:20:14 +0300

    Переверстал devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :rememberable, :validatable
 
   has_many :members, dependent: :destroy
   has_many :workspaces, through: :members, dependent: :destroy
